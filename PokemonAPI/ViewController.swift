caf59abda097fa129202e37507df5869f462965c
samberutu <45347400+samberutu@users.noreply.github.com>
2023-01-27 22:15:12 +0700

    Initial Commit

---
+++ PokemonAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonAPI
+//
+//  Created by Samlo Berutu on 27/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
